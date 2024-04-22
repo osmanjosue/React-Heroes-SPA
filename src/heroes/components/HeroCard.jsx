8da078593fefa80e5d58a5533a5543d43b9a01d3
osmanjosue <osmanjosue@gmail.com>
2024-04-22 11:41:06 -0600

    Seccion 14 => Clase: Tarjeta del Heroe-Parte 2

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,3 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+  /* if (alter_ego === characters) return (<></>);
+  return <p>{characters}</p>; */
+  return alter_ego === characters ? <></> : <p>{characters}</p>;
+};
+
 export const HeroCard = ({
   id,
   superhero,
@@ -7,6 +15,8 @@ export const HeroCard = ({
   characters,
 }) => {
   const heroImageUrl = `assets/heroes/${id}.jpg`;
+
+  // const charactersByHero = <p>{characters}</p>;
 
   return (
     <div className="col">
@@ -19,7 +29,11 @@ export const HeroCard = ({
             <div className="card-body">
               <h5 className="card-title">{superhero}</h5>
               <p className="card-text">{alter_ego}</p>
-              <p>{characters}</p>
+              <CharactersByHero characters={characters} alter_ego={alter_ego} />
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
+              <Link to={`/hero/${id}`}>Mas...</Link>
             </div>
           </div>
         </div>
--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -12,7 +12,7 @@ export const HeroesRoutes = () => {
           <Route path="dc" element={<DcPage />} />
           <Route path="search" element={<SearchPage />} />
           <Route path="hero" element={<HeroPage />} />
-          <Route path="/*" element={<Navigate to="/marvel" />} />
+          <Route path="/" element={<Navigate to="/marvel" />} />
         </Routes>
       </div>
     </>
