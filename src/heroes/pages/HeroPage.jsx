93667f30345726cbcead9ad02279a9b170974caa
osmanjosue <osmanjosue@gmail.com>
2024-04-22 12:43:40 -0600

    Seccion 14 => Clase: Leer argumentos por URL

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,15 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { getHeroById } from '../helpers';
 
 export const HeroPage = () => {
-  return (
-    <h1>HeroPage</h1>
-  )
-}
+  const { id } = useParams();
+
+  const hero = getHeroById(id);
+  console.log(hero);
+
+  if (!hero) {
+    return <Navigate to="/marvel" />;
+  }
+
+  return <h1>HeroPage</h1>;
+};
