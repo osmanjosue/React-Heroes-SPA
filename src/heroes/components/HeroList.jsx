e336e9bacd85656cfc20942e8c7d8c622f2924b0
osmanjosue <osmanjosue@gmail.com>
2024-04-22 15:15:56 -0600

    Seccion 14 => Clase: Nota: UseMemo

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,12 +1,13 @@
+import { useMemo } from 'react';
 import { getHeroesByPublisher } from '../helpers';
 import { HeroCard } from './';
 
 export const HeroList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
-    console.log(heroes)
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+  console.log(heroes);
   return (
     <div className="row rows-cols-1 row-cols-md-3 g-3">
-      {heroes.map(hero => (
+      {heroes.map((hero) => (
         <HeroCard key={hero.id} {...hero} />
       ))}
     </div>
