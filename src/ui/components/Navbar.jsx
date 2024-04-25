fec68a0eca66e3ef736cd886e5c7ee97914d457d
osmanjosue <osmanjosue@gmail.com>
2024-04-24 22:48:28 -0600

    seccion 14 => Parte2 => clase: Login de un usuario => utilizacion de los valores del useContext

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context';
 
 export const Navbar = (props) => {
+
+  const {user} = useContext(AuthContext);
 
   console.log();
 
@@ -49,7 +53,7 @@ export const Navbar = (props) => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-primary">
-            Fernando!
+            {user?.name}
           </span>
           <button className="nav-item nav-link btn" onClick={onLogout}>Logout</button>
         </ul>
