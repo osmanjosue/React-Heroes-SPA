fb0d27396442c6cc889d2d8064c8248f44f97856
osmanjosue <osmanjosue@gmail.com>
2024-05-01 13:44:42 -0600

    Se borro un console.log del HeroList

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -4,7 +4,6 @@ import { HeroCard } from './';
 
 export const HeroList = ({ publisher }) => {
   const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
-  console.log(heroes);
   return (
     <div className="row rows-cols-1 row-cols-md-3 g-3">
       {heroes.map((hero) => (
